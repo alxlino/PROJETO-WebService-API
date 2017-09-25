0e054f769984741e16104a1ca425e9c4296387f5
unknown <alx.lino@gmail.com>
2017-09-25 01:28:16 -0300

    Segundo Cenário Concluído

--- films/tests/features/specs/titulos_filmes.feature
+++ films/tests/features/specs/titulos_filmes.feature
@@ -7,3 +7,10 @@ Cenário: Consultar todos Títulos dos Filmes
     Quando envio uma solicitação GET para o serviço "http://swapi.co/api/films"
     E solicito apenas a exibição do campo "title"
     Então o código de resposta deve ser igual a "200"
+
+@doing
+Cenário: Consultar Títulos dos Filmes com Filtros de Diretor e Produtor
+
+    Quando envio uma solicitação GET para o serviço "http://swapi.co/api/films"
+    E os filmes são do diretor "George Lucas" e do produtor "Rick McCallum"
+    Então o código de resposta deve ser igual a "200"
--- films/tests/features/step_definitions/title_steps.rb
+++ films/tests/features/step_definitions/title_steps.rb
@@ -6,11 +6,27 @@ end
                                                                              
 Quando(/^solicito apenas a exibição do campo "([^"]*)"$/) do |title|          
     @data = @result.parsed_response["results"]
-    
     @data.each do |item|
-        puts item["title"]
+        puts item[title]
     end
 end                                                                          
+
+Quando(/^os filmes são do diretor "([^"]*)" e do produtor "([^"]*)"$/) do |director, producer|
+    @data = @result.parsed_response["results"]
+    
+    @data.each do |item|
+        
+        if item["director"] == director
+            str = item["producer"]
+            str = str.split(",")
+            str.each do |nome|
+                if nome.strip == producer
+                    puts item["title"]
+                end
+            end
+        end     
+    end    
+end
                                                                              
 Então(/^o código de resposta deve ser igual a "([^"]*)"$/) do |statusCode|         
   expect(@result.response.code).to eql statusCode 
